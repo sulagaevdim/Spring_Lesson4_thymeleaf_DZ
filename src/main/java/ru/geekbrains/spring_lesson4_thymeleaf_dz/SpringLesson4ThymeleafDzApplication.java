71156c486ce75e5743f18ba8a6001763e41e4aee
Дмитрий <sulagaevdim@mail.ru>
2024-10-03 20:53:14 +0400

    first commit

---
+++ src/main/java/ru/geekbrains/spring_lesson4_thymeleaf_dz/SpringLesson4ThymeleafDzApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring_lesson4_thymeleaf_dz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLesson4ThymeleafDzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLesson4ThymeleafDzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/spring_lesson4_thymeleaf_dz/SpringLesson4ThymeleafDzApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring_lesson4_thymeleaf_dz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLesson4ThymeleafDzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
